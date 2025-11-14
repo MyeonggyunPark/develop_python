86d0984a136c780fcf0c14c45e67c19a3d12ab14
Myeonggyun Park <audrbs92@gmail.com>
2025-11-14 13:16:54 +0100

    build: add Tailwind CLI configuration and generated setup files

---
+++ django_practice/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./templates/**/*.{html,js}", "./menus/templates/**/*.{html,js}"],
+  safelist: ["header-bg", "content-bg", "footer-bg"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
