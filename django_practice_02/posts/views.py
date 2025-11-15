e2cf233cb7dcc709f34dfa77cb03f02eeb73ecb0
Myeonggyun Park <audrbs92@gmail.com>
2025-11-15 21:49:03 +0100

    feat(posts): add index and post_list views for rendering templates

--- django_practice_02/posts/views.py
+++ django_practice_02/posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    return render(request, "post/index.html")
+
+
+def post_list(request):
+    
+    posts = Post.objects.all()
+    context = {
+        "posts": posts
+    }
+    
+    return render(request, "posts/post_list.html", context)
