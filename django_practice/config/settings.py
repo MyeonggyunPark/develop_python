c0bdc6e30a622091828ece622d7c42a811551309
Myeonggyun Park <audrbs92@gmail.com>
2025-11-15 13:49:24 +0100

    chore(settings): configure production settings for pythonanywhere deployment

--- django_practice/config/settings.py
+++ django_practice/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*u-ygr*#f&*0gvn5&4e_9tfacv#1&7m&-l#$kj11))9$7x81py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".pythonanywhere.com"]
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
