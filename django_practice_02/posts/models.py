108585fa8459f540f4fcbd2386a6e064fa12186a
Myeonggyun Park <audrbs92@gmail.com>
2025-11-15 20:11:28 +0100

    feat(posts): add Post model

--- django_practice_02/posts/models.py
+++ django_practice_02/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    felling_point = models.IntegerField(default=0)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
