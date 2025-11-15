5fa817ba4523db8b23a17f63b0664bb4eb395eb5
Myeonggyun Park <audrbs92@gmail.com>
2025-11-15 08:13:59 +0100

    feat(models): add Menu model definition

--- django_practice/menus/models.py
+++ django_practice/menus/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=180)
+    description = models.TextField(max_length=120)
+    price = models.IntegerField()
+    img_path = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
