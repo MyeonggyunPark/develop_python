32166aa96cac13ae08498aa81f759d4d401bc027
Myeonggyun Park <audrbs92@gmail.com>
2025-11-16 19:33:25 +0100

    feat: add PostForm to handle post input

---
+++ django_practice_02/posts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class Postform(forms.Form):
+    title = forms.CharField(max_length=100, label="Title")
+    content = forms.CharField(widget=forms.Textarea, label="Content")
+    feeling = forms.CharField(max_length=80, label="Feeling")
+    feeling_point = forms.IntegerField(min_value=0, max_value=10, label="Feeling Point")
