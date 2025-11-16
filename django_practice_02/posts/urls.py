8e9118d5b74f18a916887a84b80dda992a3437fc
Myeonggyun Park <audrbs92@gmail.com>
2025-11-16 13:46:50 +0100

    refactor: update URL pattern for post detail view

--- django_practice_02/posts/urls.py
+++ django_practice_02/posts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("posts/", views.post_list, name="post-list"),
     path("posts/new/", views.post_create, name="post-create"),
-    path("posts/<int:id>/", views.post_detail, name="post-detail"),
+    path("posts/detail/<int:id>/", views.post_detail, name="post-detail"),
     path("posts/<int:id>/edit/", views.post_update, name="post-update"),
     path("posts/<int:id>/delete/", views.post_delete, name="post-delete"),
 ]
