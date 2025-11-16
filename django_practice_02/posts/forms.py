45873a9e65780367662c91bb45e042fb79646e04
Myeonggyun Park <audrbs92@gmail.com>
2025-11-16 22:26:43 +0100

    style: add Tailwind widgets to PostForm fields

--- django_practice_02/posts/forms.py
+++ django_practice_02/posts/forms.py
@@ -7,3 +7,21 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "feeling", "feeling_point"]
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "class": "w-full p-2 border border-gray-300 rounded mt-2",
+                "placeholder": "Enter the title",
+            }),
+            "content": forms.Textarea(attrs={
+                "class": "w-full p-2 border border-gray-300 rounded mt-2",
+                "placeholder": "Enter the content",
+            }),
+            "feeling": forms.TextInput(attrs={
+                "class": "w-full p-2 border border-gray-300 rounded mt-2",
+                "placeholder": "Enter your feeling",
+            }),
+            "feeling_point": forms.NumberInput(attrs={
+                "class": "w-full p-2 border border-gray-300 rounded mt-2",
+                "placeholder": "Enter the feeling point",
+            }),
+        }
