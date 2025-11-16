8730840604d88d731cca086d9eb8a1cae79a0830
Myeonggyun Park <audrbs92@gmail.com>
2025-11-16 23:16:15 +0100

    feat: add post update view and reuse create template for editing

--- django_practice_02/posts/views.py
+++ django_practice_02/posts/views.py
@@ -4,26 +4,23 @@ from .forms import PostForm
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "posts/index.html")
 
 
 def post_list(request):
-    
+
     posts = Post.objects.all()
-    context = {
-        "posts": posts
-    }
-    
+    context = {"posts": posts}
+
     return render(request, "posts/post_list.html", context)
 
 
 def post_detail(request, id):
     post = Post.objects.get(id=id)
-    context = {
-        "post": post
-    }
-    
+    context = {"post": post}
+
     return render(request, "posts/post_detail.html", context)
 
 
@@ -33,8 +30,23 @@ def post_create(request):
         if post_form.is_valid():
             post_form.save()
             return redirect("post-list")
-    else:    
+    else:
         post_form = PostForm()
-        
-    context = {"form": post_form}    
+
+    context = {"form": post_form, "create": True}
+    return render(request, "posts/post_create.html", context)
+
+
+def post_update(request, id):
+    post = Post.objects.get(id=id)
+
+    if request.method == "POST":
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("post-detail", id=post.id)
+    else:
+        post_form = PostForm(instance=post)
+
+    context = {"form": post_form, "edit": True}
     return render(request, "posts/post_create.html", context)
