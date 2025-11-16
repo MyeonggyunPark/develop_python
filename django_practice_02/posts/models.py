aee104592582513ef572f000e64fbc21a8c231be
Myeonggyun Park <audrbs92@gmail.com>
2025-11-16 13:44:23 +0100

    fix: correct typo in model field name

--- django_practice_02/posts/models.py
+++ django_practice_02/posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     feeling = models.CharField(max_length=80)
-    felling_point = models.IntegerField(default=0)
+    feeling_point = models.IntegerField(default=0)
     dt_created = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(auto_now=True)
     
