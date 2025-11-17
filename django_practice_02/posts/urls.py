0c9ad751275975cec8bb8158d56d6996f7f61d87
Myeonggyun Park <audrbs92@gmail.com>
2025-11-17 13:24:56 +0100

    refactor: wire post-create route to PostCreateView class-based view

--- django_practice_02/posts/urls.py
+++ django_practice_02/posts/urls.py
@@ -5,7 +5,7 @@ from posts import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("posts/", views.post_list, name="post-list"),
-    path("posts/new/", views.post_create, name="post-create"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post-create"),
     path("posts/detail/<int:id>/", views.post_detail, name="post-detail"),
     path("posts/<int:id>/edit/", views.post_update, name="post-update"),
     path("posts/<int:id>/delete/", views.post_delete, name="post-delete"),
