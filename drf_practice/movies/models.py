c39196fd547a0cbc46049dff875bdf7cf357cf16
Myeonggyun Park <audrbs92@gmail.com>
2025-12-09 21:06:51 +0100

    feat: Establish one-to-many relationship between Movie and Review
    [DE] Einrichtung einer Eins-zu-Viele-Beziehung zwischen Film und Bewertung
    
    [KR] Movie-Review 1:N 관계 설정 및 related_name 적용

--- drf_practice/movies/models.py
+++ drf_practice/movies/models.py
@@ -13,7 +13,7 @@ class Movie(models.Model):
 
 
 class Review(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     username = models.CharField(max_length=30)
     star = models.IntegerField()
     comment = models.CharField(max_length=100)
--- drf_practice/movies/serializers.py
+++ drf_practice/movies/serializers.py
@@ -1,26 +1,6 @@
 from rest_framework import serializers
 from .models import Movie, Review
 
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    opening_date = serializers.DateField()
-    running_time = serializers.IntegerField()
-    overview = serializers.CharField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.opening_date = validated_data.get("opening_date", instance.opening_date)
-        instance.running_time = validated_data.get("running_time", instance.running_time)
-        instance.overview = validated_data.get("overview", instance.overview)
-        instance.save()
-        return instance
-
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
@@ -28,3 +8,35 @@ class ReviewSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "movie": {"read_only": True},
         }
+
+class MovieSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = [
+            "id",
+            "name",
+            "reviews",
+            "opening_date",
+            "running_time",
+            "overview",
+        ]
+        read_only_fields = ["reviews"]
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField()
+    # opening_date = serializers.DateField()
+    # running_time = serializers.IntegerField()
+    # overview = serializers.CharField()
+
+    # def create(self, validated_data):
+    #     return Movie.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get("name", instance.name)
+    #     instance.opening_date = validated_data.get("opening_date", instance.opening_date)
+    #     instance.running_time = validated_data.get("running_time", instance.running_time)
+    #     instance.overview = validated_data.get("overview", instance.overview)
+    #     instance.save()
+    #     return instance
+
+
