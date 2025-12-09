c5ba7fbe620106e5e70fe61b7bd488b4db02f99b
Myeonggyun Park <audrbs92@gmail.com>
2025-12-09 13:52:28 +0100

    feat: Implement movie list API with serializer, view, and URL routing
    [DE] Implementierung der Filmlisten-API mit Serializer, View und URL-Routing
    
    [KR] 영화 목록 조회 API 구현 (Serializer, View, URL 설정)

--- drf_practice/movies/views.py
+++ drf_practice/movies/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+
+from .models import Movie
+from .serializers import MovieSerializer
 
 # Create your views here.
+
+
+@api_view(["GET"])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
